1d9f4a5542776f93353c23ccf82b0a73c560b551
l0k11 <l0k1@l0k1.es>
2024-03-11 14:16:35 +0100

    l0k1.es ready for Docker deploy over HTTP

--- l0k1.es/src/config.ts
+++ l0k1.es/src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'l0k1 | Personal Portfolio Website Template';
+export const SITE_TITLE = 'l0k1 | Personal Portfolio';
 export const SITE_DESCRIPTION = 'l0k1 (loki) personal portfolio - Made using astrofy [github.com/manuelernestog/astrofy]';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
