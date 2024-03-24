fa972071d6367be14308ddda2079983c68b25084
l0k11 <l0k1@l0k1.es>
2024-03-24 15:52:46 +0100

    Fix SEO

--- l0k1.es/src/config.ts
+++ l0k1.es/src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'l0k1 | Personal Portfolio';
-export const SITE_DESCRIPTION = 'l0k1 (loki) personal portfolio - Made using astrofy [github.com/manuelernestog/astrofy]';
+export const SITE_DESCRIPTION = 'l0k1 (loki) personal portfolio - Made using Astrofy';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
